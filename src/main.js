915a7f3042487dfe59a882659ebe1d2f5093f0a3
redapp1 <ridwan.taza@gmail.com>
2019-07-26 10:38:50 +0100

    add axios to the app

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App0.vue'
 import vuetify from './plugins/vuetify';
 import router from './routes/routes'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
+  axios,
   render: h => h(App)
 }).$mount('#app')
