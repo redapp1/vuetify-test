d3fb82ad00a09bcfee393bc0cd41c57c582b059c
redapp1 <ridwan.taza@gmail.com>
2019-07-31 09:51:58 +0100

    fix routing

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,25 +10,15 @@ const routes = [
     { path: '/', component: Home},
     { path: '/quran', 
       component: Quran,
-      beforeEnter: (to, from, next) => {  
-          if (typeof to.params.page !== 'undefined') {
-              next()
-          }                                                                    
-        next('/quran/page/1')   
-      },
       children: [
-        {path: 'page', redirect: 'page/1'},
-        { 
+          { path: '/', redirect: 'page/1'},
+          { path: 'page', redirect: 'page/1'},
+          { 
             path: 'page/:page', 
             name: 'page', 
             component: Page,
-            props: (route) => ({ pageNumber: +route.params.page }),
-            beforeEnter: (to, from, next) => {
-                if (to.params.page > 0 && to.params.page < 605) next()
-    
-                next('/quran/page/1')
-              },
-        }
+            props: (route) => ({ pageNumber: +route.params.page })
+          }
       ]
     }
 ]
