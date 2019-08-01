0c8991dc73030e0dbf3610ea933b69bd1303b331
redapp1 <ridwan.taza@gmail.com>
2019-08-01 17:46:42 +0100

    fetch all founded list ayahs

--- config/config.js
+++ config/config.js
@@ -10,6 +10,10 @@ export const QURAN_EDITIONS_API = ({ version = 'v1' }) => {
     return `http://api.alquran.cloud/${version}/edition`
 }
 
+export const QURAN_SEARCH_API = () => {
+    return `http://api.alquran.cloud/v1/search/Abraham/all/en`
+}
+
 export const COUNTRIES_API = () => {
     return `https://restcountries.eu/rest/v2/all`
 }
--- src/components/Quran/Browser/FoundedAyahs.vue
+++ src/components/Quran/Browser/FoundedAyahs.vue
@@ -1,11 +1,28 @@
 <template>
     <div>
-        ayahs list
+        <ul>
+            <li v-for="ayah in ayahsList" :key="ayah.number">
+                ayah.text
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+import EventBus from '../../../event-bus/event-bus';
+import { QURAN_SEARCH_API } from '../../../../config/config'
+
 export default {
-    
+    data() {
+        return {
+            ayahsList: []
+        }
+    },
+
+    async mounted() {
+        let response = await this.axios.get(QURAN_SEARCH_API());
+
+        console.log(this.ayahsList = response.data.data.matches)
+    }
 }
 </script>
