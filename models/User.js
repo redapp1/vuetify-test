3973c9dbf42f70611313c3f1d5c8109206c8d57b
redapp1 <ridwan.taza@gmail.com>
2019-08-01 12:24:15 +0100

    add mongoose

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+    title:  String,
+    author: String,
+    body:   String,
+    comments: [{ body: String, date: Date }],
+    date: { type: Date, default: Date.now },
+    hidden: Boolean,
+    meta: {
+      votes: Number,
+      favs:  Number
+    }
+  });
+  
+exports.module = mongoose.model('User', User, 'users');
